be8de8f48355ef1e6e1aa5fbc8c24dee420a615e
fb421421 <fb421421@gmail.com>
2014-12-15 21:22:57 +0800

    创建 更新 同步文档测试

--- TravelJournalAndroid/src/com/travelover/traveljournal/MainActivity.java
+++ TravelJournalAndroid/src/com/travelover/traveljournal/MainActivity.java
@@ -1,18 +1,42 @@
 package com.travelover.traveljournal;
 
 import com.travelover.traveljournal.service.CouchbaseService;
+import com.travelover.traveljournalandroid.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity{
+	
+	private CouchbaseService couchbaseService;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		CouchbaseService.init(this);
+		couchbaseService=new CouchbaseService(this);
 		
+		setContentView(R.layout.mainview);
+		
+		Button createJournal = (Button) findViewById(R.id.createJournal);
+		createJournal.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				couchbaseService.setJournalName("journal1");
+				couchbaseService.createJournal();
+			}
+		});
+		
+		Button recordJournalNode = (Button) findViewById(R.id.recordJournalNode);
+		recordJournalNode.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				couchbaseService.recordNode();
+			}
+		});
 	}
 
 }
--- TravelJournalAndroid/src/com/travelover/traveljournal/service/CouchbaseService.java
+++ TravelJournalAndroid/src/com/travelover/traveljournal/service/CouchbaseService.java
@@ -2,10 +2,15 @@ package com.travelover.traveljournal.service;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.couchbase.lite.Context;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.android.AndroidContext;
 import com.couchbase.lite.auth.Authenticator;
@@ -15,32 +20,30 @@ import com.couchbase.lite.replicator.Replication;
 import android.app.Activity;
 import android.util.Log;
 
-
 public class CouchbaseService {
-	
-	public static final String JOURNAL_DB_NAME="journals";
 
-	private static Manager manager;
-	private static Database journalsDB;
-	private static Replication journalsDBPush;
-	private static Replication journalsDBPull;
-	private static final String TAG="CouchbaseService";
+	public static final String JOURNAL_DB_NAME = "journals";
+
+	private Manager manager;
+	private Database journalsDB;
+	private Replication journalsDBPush;
+	private Replication journalsDBPull;
+	private final String TAG = "CouchbaseService";
 	
-	
-	private CouchbaseService() {
-	}
-	
-	public static void init( Activity activity ){
+	private String journalName;
+
+	public CouchbaseService(Activity activity) {
 		try {
-			
-			/*创建数据库管理对象*/
-			manager = new Manager((Context) new AndroidContext(activity), Manager.DEFAULT_OPTIONS);
-			Log.d (TAG, "Manager created!");
-			
-			/*检查创建数据库对象*/
-			journalsDB=manager.getDatabase(JOURNAL_DB_NAME);
-			
-			URL url = new URL("http://www.en2012.com:4984/journals/");
+
+			/* 创建数据库管理对象 */
+			manager = new Manager((Context) new AndroidContext(activity),
+					Manager.DEFAULT_OPTIONS);
+			Log.d(TAG, "Manager created!");
+
+			/* 检查创建数据库对象 */
+			journalsDB = manager.getDatabase(JOURNAL_DB_NAME);
+
+			URL url = new URL("http://120.24.225.199:4984/journals/");
 			Replication push = journalsDB.createPushReplication(url);
 			Replication pull = journalsDB.createPullReplication(url);
 			pull.setContinuous(true);
@@ -48,63 +51,113 @@ public class CouchbaseService {
 			Authenticator auth = new BasicAuthenticator("test001", "111111");
 			push.setAuthenticator(auth);
 			pull.setAuthenticator(auth);
-			
+
 			push.addChangeListener(new Replication.ChangeListener() {
-			    @Override
-			    public void changed(Replication.ChangeEvent event) {
-			        // will be called back when the push replication status changes
-			    }
+				@Override
+				public void changed(Replication.ChangeEvent event) {
+					// will be called back when the push replication status
+					// changes
+					Log.d(TAG, "change count:" + event.getChangeCount());
+				}
 			});
 			pull.addChangeListener(new Replication.ChangeListener() {
-			    @Override
-			    public void changed(Replication.ChangeEvent event) {
-			        // will be called back when the pull replication status changes
-			    }
+				@Override
+				public void changed(Replication.ChangeEvent event) {
+					// will be called back when the pull replication status
+					// changes
+					Log.d(TAG, "change count:" + event.getChangeCount());
+				}
 			});
 			push.start();
 			pull.start();
 			journalsDBPush = push;
 			journalsDBPull = pull;
-			
+
 		} catch (IOException e) {
-		    Log.e(TAG, "Cannot create manager object");
-		    throw new RuntimeException();
+			Log.e(TAG, "Cannot create manager object");
+			throw new RuntimeException();
 		} catch (CouchbaseLiteException e) {
 			Log.e(TAG, "Cannot create manager object");
 			throw new RuntimeException();
 		}
 	}
 
-	public static Manager getManager() {
+	public String createJournal() {
+		Document document = this.journalsDB.getDocument(this.journalName);
+		Log.d(TAG, "create journal:" + document.getId());
+		return document.getId();
+	}
+
+	public void recordNode() {
+
+		Document journal = this.journalsDB.getDocument(this.journalName);
+
+		if (journal == null) {
+			Log.e(TAG, "can not find journal:" + this.journalName);
+			return;
+		}
+
+		// update the document
+		Map<String, Object> updatedProperties = new HashMap<String, Object>();
+		updatedProperties.putAll(journal.getProperties());
+
+		@SuppressWarnings("unchecked")
+		List<String> locationList = (List<String>) updatedProperties
+				.get("locationlist");
+		if (locationList == null) {
+			locationList = new ArrayList<String>();
+		}
+		locationList.add(String.valueOf(System.currentTimeMillis()));
+		updatedProperties.put("locationlist", locationList);
+
+		try {
+			journal.putProperties(updatedProperties);
+			Log.d(TAG,
+					"updated retrievedDocument="
+							+ String.valueOf(journal.getProperties()));
+		} catch (CouchbaseLiteException e) {
+			Log.e(TAG, "Cannot update document", e);
+		}
+	}
+
+	public Manager getManager() {
 		return manager;
 	}
 
-	public static void setManager(Manager manager) {
-		CouchbaseService.manager = manager;
+	public void setManager(Manager manager) {
+		this.manager = manager;
 	}
 
-	public static Database getJournalsDB() {
+	public Database getJournalsDB() {
 		return journalsDB;
 	}
 
-	public static void setJournalsDB(Database journalsDB) {
-		CouchbaseService.journalsDB = journalsDB;
+	public void setJournalsDB(Database journalsDB) {
+		this.journalsDB = journalsDB;
 	}
 
-	public static Replication getJournalsDBPush() {
+	public Replication getJournalsDBPush() {
 		return journalsDBPush;
 	}
 
-	public static void setJournalsDBPush(Replication journalsDBPush) {
-		CouchbaseService.journalsDBPush = journalsDBPush;
+	public void setJournalsDBPush(Replication journalsDBPush) {
+		this.journalsDBPush = journalsDBPush;
 	}
 
-	public static Replication getJournalsDBPull() {
+	public Replication getJournalsDBPull() {
 		return journalsDBPull;
 	}
 
-	public static void setJournalsDBPull(Replication journalsDBPull) {
-		CouchbaseService.journalsDBPull = journalsDBPull;
+	public void setJournalsDBPull(Replication journalsDBPull) {
+		this.journalsDBPull = journalsDBPull;
+	}
+
+	public String getJournalName() {
+		return journalName;
+	}
+
+	public void setJournalName(String journalName) {
+		this.journalName = journalName;
 	}
 
 }
